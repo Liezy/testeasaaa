1a9ae5928bcf62496ae38752643e9da742d92e23
Pedro <pedrolmoreira99@gmail.com>
2026-02-11 15:22:39 -0300

    feat: feat

--- releases/models.py
+++ releases/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 class Release(models.Model):
     tag = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=100)
     body = models.TextField()
-    published_at = models.DateTimeField()
+    published_at = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
