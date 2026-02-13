d102ffae47210308e78aada257604c82f0897f3b
Pedro <pedrolmoreira99@gmail.com>
2026-02-13 17:01:43 -0300

    qwert

--- releases/services.py
+++ releases/services.py
@@ -25,7 +25,7 @@ def generate_user_notes(changelog_text: str) -> str:
 
 
     response = client.chat.completions.create(
-        model="mixtral-8x7b-32768",
+        model="llama-3.1-8b-instant",
         messages=[
             {"role": "user", "content": prompt}
         ],
